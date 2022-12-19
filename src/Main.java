8b09a60ff7f01130f0928295ed32b03033a707c2
tatianahaughton <tatianahaughton33@gmail.com>
2022-12-19 14:28:49 -0500

    Feature sun has been added but has some errors

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-
-
         SolarSystem xx = new SolarSystem();
         xx.moon = 1;
         xx.sun = 1;
         xx.planet=2;
+
+        FeatureSUN Xeron=new FeatureSUN();
+        Xeron.Heat="1232434";
+        Xeron.size=12343445;
+        Xeron.Temperature="3434";
     }
 }
